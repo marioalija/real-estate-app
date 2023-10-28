598c8ed2e8e43d049893d648b6d867cbe1504738
marioalija <marioalija25@gmail.com>
2023-10-28 12:15:42 -0500

    -m

--- db/migrate/20231027200843_remove_floors_from_homes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFloorsFromHomes < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :homes, :floors
-  end
-end
--- db/migrate/20231027202403_add_floors_to_homes.rb
+++
@@ -1,5 +0,0 @@
-class AddFloorsToHomes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :homes, :floors, :integer
-  end
-end
